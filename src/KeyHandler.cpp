a92990f6ffa6762c458ddd80e3c590a31bd82273
Lukhnos Liu <lukhnos@lukhnos.org>
2026-08-06 23:19:38 -0700

    Use correct walk snapshots for UOM observation
    UOM needs both the previous walk result and the current walk result
    after node override (that is, after user manually chooses a candidate)
    to make an observation, but since the nodes in a WalkResult are shared
    pointers to the Node instances in the grid, WalkResult does not capture
    an immutable snapshot. A new member function is introduced to enable
    getting a snapshot that has a real copy of the nodes, so as to capture
    the state of the grid before it is further mutated.

--- src/KeyHandler.cpp
+++ src/KeyHandler.cpp
@@ -1777,6 +1777,14 @@ void KeyHandler::pinNode(
     size_t originalCursor,
     const InputStates::ChoosingCandidate::Candidate& candidate,
     bool useMoveCursorAfterSelectionSetting) {
+  // Since WalkResult makes references to the current nodes, we must make a
+  // copy of the walk that *has a copy* of the current nodes to capture the
+  // current state. ReadingGrid::overrideCandidate() changes the state, and
+  // so having a simple copy of latestWalk_ (auto prevWalk = latestWalk_;)
+  // is NOT enough.
+  Formosa::Gramambular2::ReadingGrid::WalkResult prevWalk =
+      latestWalk_.copyWithFixedNodes();
+
   size_t actualCursor = actualCandidateCursorIndex();
   Formosa::Gramambular2::ReadingGrid::Candidate gridCandidate(
       candidate.reading, candidate.value, "");
@@ -1784,7 +1792,6 @@ void KeyHandler::pinNode(
     return;
   }
 
-  Formosa::Gramambular2::ReadingGrid::WalkResult prevWalk = latestWalk_;
   walk();
 
   // Update the user override model if warranted.
